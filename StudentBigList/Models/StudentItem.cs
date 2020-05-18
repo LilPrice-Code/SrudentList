bc23dd4d9c8db019ccdefdb15a9922de33749965
LilPrice-Code <littleprice002@gmail.com>
2020-05-18 14:38:16 -0500

    Updated 5/18

--- StudentBigList/Models/StudentItem.cs
+++ StudentBigList/Models/StudentItem.cs
@@ -1,5 +1,10 @@
+// Setting the Class for API
+// Public Class to access the information within the scope of the Project.
+// Private Class accessed only in the class.
 public class StudentItem
 {
+    // Public attributes access in project
+    // Private attributes access in class
     public long Id { get; set; }
     public string Name { get; set; }
     public int Student_Id { get; set; }
